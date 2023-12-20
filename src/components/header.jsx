ac0149c727171781206d6df05cd1f49c3a8ae1bb
Joe Molina <joedodaniljr123@gmail.com>
2023-12-20 01:43:58 -0400

    inicio hasta menu 3 cards

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+export const HeaderNav = () => {
+  return (
+    <header className="bg-white h-20 flex items-center justify-between px-5">
+      <img src="/quieroGalletaLogo.svg" className="h-10" alt="" />
+
+      <a href="" className="bg-red-300 p-3 rounded-full">
+        <img src="/Caritafeli.svg" alt="" className="h-8" />
+      </a>
+    </header>
+  );
+};
