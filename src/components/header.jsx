2fdb7c7dd49b91bf8a4ca8c9ed2c2f4bfc63f97e
Joe Molina <joedodaniljr123@gmail.com>
2023-12-20 12:11:19 -0400

    top galletas component add

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 export const HeaderNav = () => {
   return (
-    <header className="bg-white h-20 flex items-center justify-between px-5">
+    <header className="bg-white h-20 flex items-center justify-between px-5 fixed top-0 z-50 w-full">
       <img src="/quieroGalletaLogo.svg" className="h-10" alt="" />
 
       <a href="" className="bg-red-300 p-3 rounded-full">
