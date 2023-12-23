b9f2f9f508270d048a8eee0e8c5be84f69cf045d
Joe Molina <joedodaniljr123@gmail.com>
2023-12-23 12:59:59 -0400

    menu de galletas con componentes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+
 export const HeaderNav = () => {
+  const [getMenu, setGetMenu] = useState(false);
+
+  const handleClick = () => {
+    setGetMenu(!getMenu);
+  };
+
+  const buttonGetMenu = getMenu
+    ? "bg-blue-300 p-3 rounded-full"
+    : "bg-red-300 p-3 rounded-full";
+
+  const menuOpen = getMenu ? (
+    <article className="fixed w-screen h-full z-20 bg-blue-100 top-20 left-0 flex justify-center items-center">
+      <nav className=" absolute flex flex-col justify-start items-center h-full   w-full text-center pt-16">
+        <a href="/MenuGalletas" className=" text-2xl py-10  hover:bg-blue-400">
+          Inicio
+        </a>
+        <a href="" className=" text-2xl py-10  hover:bg-blue-400">
+          Galletas
+        </a>
+        <a href="" className=" text-2xl py-10  hover:bg-blue-400">
+          Brownies
+        </a>
+        <a href="" className=" text-2xl py-10  hover:bg-blue-400">
+          Bebidas
+        </a>
+      </nav>
+    </article>
+  ) : (
+    <></>
+  );
+
   return (
     <header className="bg-white h-20 flex items-center justify-between px-5 fixed top-0 z-50 w-full">
       <img src="/quieroGalletaLogo.svg" className="h-10" alt="" />
 
-      <a href="" className="bg-red-300 p-3 rounded-full">
+      <button href="" className={buttonGetMenu} onClick={handleClick}>
         <img src="/Caritafeli.svg" alt="" className="h-8" />
-      </a>
+      </button>
+      {menuOpen}
     </header>
   );
 };
