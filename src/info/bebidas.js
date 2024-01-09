da90801c3d2b61635596a28000a9956b355ca05d
Joe Molina <joedodaniljr123@gmail.com>
2024-01-08 21:13:08 -0400

    add bebidas.js

---
+++ src/info/bebidas.js
@@ -0,0 +1,204 @@
+export const CafeCaliente = [
+  {
+    name: "Espresso",
+    details: "",
+    price: ["1,30$"],
+  },
+  {
+    name: "Americano",
+    details: "Agua caliente + Espresso",
+    price: ["1,00$"],
+  },
+  {
+    name: "Macchiato",
+    details: "Espuma de leche + Espresso",
+    price: ["1,50$"],
+  },
+  {
+    name: "Latte",
+    details: "Espuma de leche + leche vaporizada + Espresso",
+    price: ["1,50$"],
+  },
+  {
+    name: "Latte vainilla",
+    details: "Espuma de leche + leche vaporizada + Espresso con vainilla",
+    price: ["1,80$"],
+  },
+  {
+    name: "Capuccino",
+    details: "Espuma de leche + Leche vaporizada + Espresso",
+    price: ["1,50$"],
+  },
+  {
+    name: "Mocaccino",
+    details: "Leche vaporizada + Espresso con cacao",
+    price: ["1,80$"],
+  },
+  {
+    name: "Bombon moca",
+    details: "Espuma de leche + Espresso con cacao + Leche condensada",
+    price: ["3,00$"],
+  },
+  {
+    name: "Bombon Galleta",
+    details:
+      "Espuma de leche + Espresso + Leche condensada + Trozos de galleta.",
+    price: ["3,00$"],
+  },
+  {
+    name: "Bombon Nutella",
+    details: "Espuma de leche + Espresso + Leche condensada + Nutella",
+    price: ["3,00$"],
+  },
+];
+
+export const CafeFrio = [
+  {
+    name: "Quiero Galleta",
+    details:
+      "Contiene sirope de galleta + Trozos de galleta monstruosamente deliciosa",
+    price: "4,50$",
+  },
+  {
+    name: "Cookies and Cream",
+    details:
+      "Contiene sirope de chocolate blanco + nuestra galleta espacial de cookies and cream + oreo",
+    price: "4,50$",
+  },
+  {
+    name: "Nutella",
+    details: "Contiene sirope de avellana + Nutella",
+    price: "4,50$",
+  },
+  {
+    name: "Caramelo salado",
+    details:
+      "Contiene sirope de caramelo salado + trozos de galleta crujiente de caramelo",
+    price: "4,50$",
+  },
+  {
+    name: "Moca",
+    details:
+      "¡Es nuestro cafe frio mas cremoso! Contiene chocolate y cafe por partes iguales",
+    price: "4,50",
+  },
+  {
+    name: "Caramelo",
+    details: "Contiene sirope de caramelo sin azucar",
+    price: "???",
+    add: "sin azucar",
+  },
+  {
+    name: "Avellana",
+    details: "Contiene sirope de avellana sin azucar",
+    price: "???",
+    add: "sin azucar",
+  },
+  {
+    name: "Vainilla",
+    details: "Contiene sirope de vainilla sin azucar",
+    price: "???",
+    add: "sin azucar",
+  },
+];
+
+export const Infusiones = [
+  {
+    name: "Matcha latte caliente",
+    price: ["4,00$"],
+  },
+  {
+    name: "Matcha latte frio",
+    details:
+      "Te verde matcha con leche. Pidelo preferiblemente con leche de coco",
+    price: ["4,00$"],
+  },
+  {
+    type: "Tisana & Blends",
+    name: "Madame Butterfly",
+    details: "Flores de clitoria ternatea y hojas de menta",
+    price: ["Frio: 4,50$", "Caliente: 3,00$"],
+  },
+  {
+    type: "Tisana & Blends",
+    name: "Blueberry wine",
+    details:
+      "Trozos de manzana, flor de caena, petalos de aciano y frutos rojos",
+    price: ["Frio: 4,50$", "Caliente: 3,00$"],
+  },
+  {
+    type: "Te verde",
+    name: "Green Strawberry",
+    details: "Te verde grado sencha con trocitos de fresa y lechosa",
+    price: ["Frio: 4,50$", "Caliente: 3,00$"],
+  },
+  {
+    type: "Favoritos",
+    name: "Arnold Palmer",
+    details:
+      "Base de agua, endulzada con almibar realizado con limon y cobertura de te negro (tropical morning)",
+  },
+  {
+    type: "Favoritos",
+    name: "Siempre Verano",
+    details:
+      "Base de te buttefly, endulzado con almibar, realizado con limon y cobertura de te strawberry",
+  },
+
+  {
+    type: "Tisana & Blends",
+    name: "Berry melody",
+    details:
+      "Una infusion afrutada, ligeramente acida y dulce. Frutos de bosque, grosellas y bayas",
+  },
+  {
+    type: "Tisana & Blends",
+    name: "Rooibos Lemon",
+    details:
+      "Infusion herbal con sabor a limon. Rooibos, con hierba y petalos de calendula",
+  },
+  {
+    type: "Te verde",
+    name: "Sweet Mango",
+    details:
+      "Te verde estilo japones con trocitos de piña, mango y flores malva",
+  },
+  {
+    type: "Te negro",
+    name: "Tropical Morning",
+    details: "Te negro con guayaba, lechosa y petalos de cartamo",
+  },
+];
+
+export const OtrasBebidas = [
+  {
+    name: "Vaso con leche",
+    name: "1,50$",
+  },
+  {
+    name: "Agua mineral",
+    name: "1,50$",
+  },
+  {
+    name: "Agua con gas",
+    name: "2,00$",
+  },
+  {
+    name: "Te lipton",
+    name: "2,50$",
+  },
+  {
+    name: "Refresco",
+    name: "2,50$",
+  },
+  {
+    name: "Chocolate Caliente",
+    name: "2,50$",
+  },
+  {
+    name: "Toddy Espacial",
+    details:
+      "No es el Toddy que te preparas en tu casa, ¡es el mejor de todos!",
+    price: "4,50$",
+  },
+];
