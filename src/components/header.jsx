08caae012a2f6b056d49c251e73ecf43d4ce8adf
Joe Molina <joedodaniljr123@gmail.com>
2024-01-11 21:12:41 -0400

    desktop mode listo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,8 +8,8 @@ export const HeaderNav = () => {
   };
 
   const buttonGetMenu = getMenu
-    ? "bg-blue-300 p-3 rounded-full"
-    : "bg-red-300 p-3 rounded-full";
+    ? "bg-blue-300 p-3 rounded-full lg:hidden"
+    : "bg-red-300 p-3 rounded-full lg:hidden";
 
   const menuOpen = getMenu ? (
     <article className="fixed w-screen h-full z-20 bg-blue-100 top-20 left-0 flex justify-center items-center">
@@ -44,22 +44,43 @@ export const HeaderNav = () => {
     <></>
   );
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentPosition = document.documentElement.scrollTop;
-      if (currentPosition > 0) {
-        console.log("coito");
-      }
-    };
-  });
-
   return (
-    <header className="bg-white h-20 flex items-center justify-between px-5 fixed top-0 z-50 w-full">
-      <a href="../../" className="bg-red-300 rounded-full p-1">
-        <img src="/arrow.svg" alt="" className="w-10   " />
+    <header className="bg-white h-20 flex items-center justify-between px-5 fixed top-0 z-50 w-full lg:shadow-md">
+      <a
+        href="javascript:history.back()"
+        className="bg-red-300 rounded-full p-1 lg:hidden"
+      >
+        <img src="/arrow.svg" alt="" className="w-10" />
       </a>
 
       <img src="/quieroGalletaLogo.svg" className="h-10" alt="" />
+
+      <nav className="hidden lg:flex">
+        <a
+          href="/"
+          className=" text-xl my-6 p-4 rounded-full hover:text-red-400 transition"
+        >
+          Inicio
+        </a>
+        <a
+          href="/Menu/Galletas"
+          className=" text-xl my-6 p-4 rounded-full hover:text-red-400 transition"
+        >
+          Galletas
+        </a>
+        <a
+          href="/Menu/Brownies"
+          className=" text-xl my-6 p-4 rounded-full hover:text-red-400 transition"
+        >
+          Brownies
+        </a>
+        <a
+          href="/Menu/Bebidas"
+          className=" text-xl my-6 p-4 rounded-full hover:text-red-400 transition "
+        >
+          Bebidas
+        </a>
+      </nav>
 
       <button href="" className={buttonGetMenu} onClick={handleClick}>
         <img src="/Caritafeli.svg" alt="" className="h-8" />
