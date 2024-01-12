a184cdd1dafb0ac9670073671ba6984a16d255d4
Joe Molina <joedodaniljr123@gmail.com>
2024-01-12 11:20:22 -0400

    v1 lista

--- src/info/brownies.js
+++ src/info/brownies.js
@@ -1,21 +1,21 @@
 export const brownies = [
   {
-    img: "/Brownies/brookieDeNutellaSalMarina.png",
+    img: "/Brownies/salmarina.png",
     title: "Brookie de nutella con sal marina",
     text: "130gr de brownie fudge + 70gr de galleta de nutella con sal marina.",
   },
   {
-    img: "/Brownies/brookieDeChispasDeChocolate.png",
+    img: "/Brownies/chispaschocolate.png",
     title: "Brookie de chispas de chocolate",
     text: "130gr de brownie fudge + 70gr de galleta de chispas de chocolate.",
   },
   {
-    img: "/Brownies/BrookieChispasNuez.png",
+    img: "/Brownies/nuez.png",
     title: "Brookie de chispas y nuez",
     text: "130gr de brownie fudge + 70gr de galleta de chispas de chocolate y trozos de nuez.",
   },
   {
-    img: "/Brownies/BigBrookie1.png",
+    img: "/Brownies/big.png",
     title: "Brookie grande",
     text: "Solo por encargo - tamaño 20x20cm - peso 800gr     Pidelo del sabor de tu galleta favorita.",
   },
